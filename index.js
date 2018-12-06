04ece3b99210143aa71523e0aa74f5eeac279985
SirSagramor <sir.sagramor@gmail.com>
2018-12-06 14:08:24 +0200

    Change last to global

--- index.js
+++ index.js
@@ -3,18 +3,15 @@
 const binance = require('node-binance-api')();
 const coins = ['XRPBTC', 'XRPETH', 'ETHBTC'];
 
-binance.websockets.depth(coins, (() => {
-  const last = {};
-  for (let i = 0; i < coins.length; i++) last[coins[i]] = [];
-  return depth => {
-    const symbol = depth.s;
-    const ask = depth.a[0] === undefined ? last[symbol][0] : depth.a[0];
-    const bid = depth.b[0] === undefined ? last[symbol][1] : depth.b[0];
-    last[symbol] = [ask, bid];
-    console.dir({ [symbol]: { ask, bid } });
-  };
-}
-)());
+const last = {};
+for (let i = 0; i < coins.length; i++) last[coins[i]] = [];
 
+binance.websockets.depth(coins, depth => {
+  const symbol = depth.s;
+  const ask = depth.a[0] === undefined ? last[symbol][0] : depth.a[0];
+  const bid = depth.b[0] === undefined ? last[symbol][1] : depth.b[0];
+  last[symbol] = [ask, bid];
+  console.dir({ [symbol]: { ask, bid } });
+});
 
 
