119232ae4d633860d57fb133e6cb5a580d40dbae
SirSagramor <sir.sagramor@gmail.com>
2018-12-06 14:06:03 +0200

    Add binance sockets bid ask

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('kek');
+const binance = require('node-binance-api')();
+const coins = ['XRPBTC', 'XRPETH', 'ETHBTC'];
+
+binance.websockets.depth(coins, (() => {
+  const last = {};
+  for (let i = 0; i < coins.length; i++) last[coins[i]] = [];
+  return depth => {
+    const symbol = depth.s;
+    const ask = depth.a[0] === undefined ? last[symbol][0] : depth.a[0];
+    const bid = depth.b[0] === undefined ? last[symbol][1] : depth.b[0];
+    last[symbol] = [ask, bid];
+    console.dir({ [symbol]: { ask, bid } });
+  };
+}
+)());
+
+
+
